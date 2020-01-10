086d7f8b5d1cbc73cfea7c64d14cb7a64a4bfb1a
Bartek <bszczypka@nano-games.com>
2020-01-10 18:19:31 +0100

    Finished work with bowling

---
+++ Assets/Scripts/SceneResetter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class SceneResetter : MonoBehaviour
+{
+    public GameObject Ball;
+
+    public GameObject Kegels;
+    public GameObject KegelsPrefab;
+
+    private Transform _kegelsSpawnPos;
+    private Vector3 _ballSpawnPos;
+
+    public SteamVR_Input_Sources HandType;
+    public SteamVR_Action_Boolean GripPressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _kegelsSpawnPos = Kegels.transform;
+        _ballSpawnPos = Ball.transform.position;
+    }
+
+    private bool CheckGripPressed()
+    {
+        return GripPressed.GetState(HandType);
+    }
+
+    private void Update()
+    {
+        if(CheckGripPressed())
+        {
+            Ball.transform.position = _ballSpawnPos;
+            var rb = Ball.GetComponentInChildren<Rigidbody>();
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            Destroy(Kegels);
+            Kegels = Instantiate(KegelsPrefab, _kegelsSpawnPos);
+        }
+    }
+}
